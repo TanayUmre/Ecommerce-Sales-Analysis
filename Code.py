306aeab6dfcaf968c963857889a460627df24222
TanayUmre <tanayumre@gmail.com>
2025-02-22 21:33:44 +0530

    Second Commit

--- Code.py
+++ Code.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-import Matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-file="C:\Users\asus\Desktop\ITW Lab Assignment\Ecommerce-Sales-Analysis\ecommercedashboard\Ecommerce Sales Analysis.xlsx"
+file="C:\\Users\\asus\\Desktop\\ITW Lab Assignment\\Ecommerce-Sales-Analysis\\ecommercedashboard\\Ecommerce Sales Analysis.xlsx"
 df=pd.read_excel(file)
 
 df["Order Date"]=pd.to_datetime(df["Order Date"],errors='coerce')
@@ -11,3 +11,13 @@ df.drop_duplicates(inplace=True)
 df.fillna(0,inplace=True)
 
 # This code is used for cleaning the data to get better output
+
+totalsale=df["Sales"].sum()
+totalprofit=df["Profit"].sum()
+totalorder=df["Order ID"].nunique()
+totalcustomer=df["Customer ID"].nunique()
+
+print(f"Total Sales are : {totalsale:.4f}")
+print(f"Total Profit is : {totalprofit:.4f}")
+print(f"Totals Orders Placed are : {totalorder}")
+print(f"Total Customers Using Ecommerce platform are : {totalcustomer}")
